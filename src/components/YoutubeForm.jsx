16a13d00e3ce5683d6bcf99637c54f5312480438
hardikd-zignuts <hardikd@zignuts.com>
2023-02-23 11:24:43 +0530

    Manage Error handling in Formik

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,16 +1,39 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, useFormik } from "formik";
+
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "Hardik4U",
+};
+
+const onSubmit = (values) => {
+  console.log("Form Values", values);
+};
+
+
+const validate = (values) => {
+  let errors = {};
+  if (!values.name) {
+    errors.name = "Required";
+  }
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email format";
+  }
+  if (!values.channel) {
+    errors.channel = "Required";
+  }
+
+  return errors;
+};
 
 const YoutubeForm = () => {
   const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      channel: "Hardik4U",
-    },
-    onSubmit: (values) => {
-      console.log("Form Values", values);
-    },
+    initialValues,
+    onSubmit,
+    validate,
   });
 
   return (
